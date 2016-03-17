f6832bfb4d2cf4e55549551c323c361c08e00fe4
Andrew <Andrew7654@hotmail.com>
2016-03-17 11:51:32 +1100

    add font to display score

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -70,6 +70,7 @@ namespace CardGames
 				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
 				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 521, 153);
+				SwinGame.DrawText ("" + myGame.Score (0), Color.White, "GameFont", 0, 30);
 			}
 			else
 			{
